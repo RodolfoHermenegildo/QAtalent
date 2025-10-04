7713504616975313132edf9f6e1eacffcd475008
RodolfoHermenegildo <rhermenegildo.g@hotmail.com>
2025-10-04 08:14:02 -0300

    Nesta atualizaçãofoi feito a separação dos dados gerados para o arquivo gerador_dados_aleatórios,py

--- Gerador_Arquivo_CSV.py
+++ Gerador_Arquivo_CSV.py
@@ -1,53 +1,23 @@
 # Biblioteca 
 from datetime import datetime
+import gerador_dados_aleatórios as gda
 import random
 
-# Funções
-
-def gerar_cpf():
-    # Gera os 9 primeiros dígitos do CPF
-    cpf = [random.randint(0, 9) for _ in range(9)]
-
-    # Calcula o primeiro dígito verificador
-    soma = sum((10 - i) * cpf[i] for i in range(9))
-    primeiro_digito = (soma * 10 % 11) % 10
-    cpf.append(primeiro_digito)
-
-    # Calcula o segundo dígito verificador
-    soma = sum((11 - i) * cpf[i] for i in range(10))
-    segundo_digito = (soma * 10 % 11) % 10
-    cpf.append(segundo_digito)
-
-    # Retorna o CPF formatado
-    return "{}{}{}{}{}{}{}{}{}{}{}".format(*cpf)
-
-def data_nasc():
-    dia = random.randint(1, 31)
-    mes = random.randint(1, 12)
-    ano = random.randint(1960, 2000)
-
-    if dia <= 9:
-        dia = (f'0{dia}')
-    if mes <= 9:
-        mes = (f'0{mes}')
-        return (f'{dia}{mes}{ano}')
-    else:
-        return (f'{dia}{mes}{ano}')
-
-
-# Dados a serem usados no arquivo
+# Dados a serem usados no arquivo:
 
 data_atual = datetime.now().strftime('%d%m%Y')
-nome = 'Rodolfo'
-data_nascimento = data_nasc()
-cpf = gerar_cpf()
-nome_mãe = ''
-endereço = ''
-cel = ''
+nome = gda.nome()
+data_nascimento = gda.data_nasc()
+cpf = gda.gerar_cpf()
+nome_mae = gda.nome()
+nome_pai = gda.nome()
+endereco = ""
+cel = ""
 
 
 
-info_csv = (f'{data_atual},{nome},{data_nascimento},{cpf},{nome_mãe},{endereço},{cel}')
+info_csv = (f'{data_atual}, {nome}, {data_nascimento}, {cpf}, {nome_mae}, {nome_pai}, {endereco},{cel}')
+print(info_csv)
 
 
 
---
+++ gerador_dados_aleatórios.py
@@ -0,0 +1,59 @@
+#==================================================================================================================================================================================== FUNÇÕES ======================================================================
+
+# Bibliotecas:
+import random
+
+# Gerar um CPF aleatório:
+
+def gerar_cpf():
+    # Gera os 9 primeiros dígitos do CPF
+    cpf = [random.randint(0, 9) for _ in range(9)]
+
+    # Calcula o primeiro dígito verificador
+    soma = sum((10 - i) * cpf[i] for i in range(9))
+    primeiro_digito = (soma * 10 % 11) % 10
+    cpf.append(primeiro_digito)
+
+    # Calcula o segundo dígito verificador
+    soma = sum((11 - i) * cpf[i] for i in range(10))
+    segundo_digito = (soma * 10 % 11) % 10
+    cpf.append(segundo_digito)
+
+    # Retorna o CPF formatado
+    return "{}{}{}{}{}{}{}{}{}{}{}".format(*cpf)
+
+
+#=================================================================================================================================
+
+# Gera uma data de nascimento aleatória:
+
+def data_nasc():
+    dia = random.randint(1, 31)
+    mes = random.randint(1, 12)
+    ano = random.randint(1960, 2000)
+
+    if dia <= 9:
+        dia = (f'0{dia}')
+    if mes <= 9:
+        mes = (f'0{mes}')
+        return (f'{dia}{mes}{ano}')
+    else:
+        return (f'{dia}{mes}{ano}')
+    
+    
+#=================================================================================================================================
+
+def nome():
+    primeiro_nome = ["Rodolfo", "Marcelli", "Guilherme", "Kauan", "Davi", "Marina", "Joana", "Damião", "Sandra", "Solange", "Simone", "Silvia", "Alvaro", "Carlinhos", "Silvana", "Barbara", "Raphaelli", "Emerson", "Fabiana", "Nicole", "Nicolas", "Luan", "Thiago", "Jarlei", "Marcos", "Kaique", "Sthephanie", "Odila", "José", "João", "Irami"]
+    p_n = random.choice(primeiro_nome)
+    segundo_nome = ["Gonçalves", "Christine", "Maia", "Albuquerque", "Mendonça", "Borges", "Senna", "Santiago", "Benício", "Catarina"]
+    s_n = random.choice(segundo_nome)
+    sobrenome = ["Hermengildo", "Silva", "Camarotto", "Souza", "Lopes", "Lelis", "Costa", "Neto", "Filho", "Júnior", "Braga", "Plinio", "Fausto", "Liberatto", "Cechetto"]
+    s = random.choice(sobrenome)
+
+    return (f'{p_n} {s_n} {s}')
+
+
+
+
+#=================================================================================================================================
