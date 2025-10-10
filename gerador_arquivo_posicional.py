6e054dcc17b580471a0a75888549c52d6f3a7472
RodolfoHermenegildo <rhermenegildo.g@hotmail.com>
2025-10-10 16:19:44 -0300

    gerador_arquivo_posicional
    Atualização dos dados para gerar o arquivo.

--- gerador_arquivo_posicional.py
+++ gerador_arquivo_posicional.py
@@ -5,9 +5,14 @@ import gerador_dados_aleatórios as gda
 # Dados a serem usados no arquivo:
 
 data_atual = datetime.now().strftime('%d%m%Y')
-data_atual_posicional = f'{data_atual:<15}'
+data_atual_posicional = (f'{data_atual:<15}')
+
 nome = gda.nome()
+nome_posicional = (f'{nome:<40}')
+
 data_nascimento = gda.data_nasc()
+data_nascimento_posicional = (f'{data_nascimento:<15}')
+
 cpf = gda.gerar_cpf()
 nome_mae = gda.nome()
 nome_pai = gda.nome()
@@ -18,7 +23,7 @@ endereco = gda.endereco()
 
 
 
-info_posicional = (f'{data_atual_posicional}{nome}{data_nascimento}{cpf}{nome_mae}{nome_pai}{endereco}{cel}{f'{email}@main.com'}{endereco}''\n')
+info_posicional = (f'{data_atual_posicional}{nome_posicional}{data_nascimento_posicional}{cpf}{nome_mae}{nome_pai}{endereco}{cel}{f'{email}@main.com'}{endereco}''\n')
 
 
 #{'São Paulo':<15}
